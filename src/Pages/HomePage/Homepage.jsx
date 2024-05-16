9829336b9fbb43694995971cbe870f51de3a59d8
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-05-16 17:21:43 +0200

    Homepage, je n'ai pas encore fini

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -1,9 +1,32 @@
+import "./homepage.css";
 
-function HomePage(){
-    return (
-        <div id="homepage">
-            homepage
+function HomePage() {
+  return (
+    <div id="homepage">
+      <h1 className="title">Welcome</h1>
+      <h1 className="title">User</h1>
+
+      <section className="grid-container">
+        <div className="item">
+          <h2 className="title2">Your </h2>
+          <h2 className="title2"> Notes</h2>
         </div>
-    )
+        <div className="item">
+          <h2 className="title2">Your </h2>
+          <h2 className="title2"> Music</h2>
+        </div>
+
+        <div className="item">
+          <h2 className="title2">Your </h2>
+          <h2 className="title2"> Quotes</h2>
+        </div>
+
+        <div className="item">
+          <h2 className="title2">Your </h2>
+          <h2 className="title2"> Images</h2>
+        </div>
+      </section>
+    </div>
+  );
 }
-export default HomePage
+export default HomePage;
